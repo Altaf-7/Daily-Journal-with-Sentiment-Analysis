8cb1effade4a3501125166bc368fb6868e11fa36
Altaf-7 <altafraja010789@gmail.com>
2024-11-12 13:02:54 +0530

    made interactive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 import datetime
 
 class Journal:
@@ -11,7 +12,7 @@ class Journal:
 
 
     def write_entry(self,entryDate,journal):
-        with open(self.file_name,'a', newline='') as file:
+        with open(self.file_name,'a', newline='\n') as file:
             writer = csv.DictWriter(file, fieldnames= ["date", "dailyJournal"])
             writer.writerow({"date": entryDate, "dailyJournal" : journal})
 
@@ -21,23 +22,45 @@ class Journal:
             reader = csv.DictReader(file)
             for row in reader:
                 if row["date"] == seachDate:
-                    print(row["dailyJournal"])
-
-
+                    return row["dailyJournal"]
+            return None
 
 
 def main():
-    date1 = '17-11-2024'
-    note1 = "today i am feeling good."
-    date2 = '18-11-2024'
-    note2 = "today i am feeling okay."
-    date3 = '19-11-2024'
-    note3 = "today i am feeling bad."
+    while True:
+        print("\nJournal Menu:")
+        print("1. Write a new entry")
+        print("2. Read previous entries")
+        print("3. Exit")
 
-    journal = Journal()
-    # journal.write_entry(date2,note2)
-    # journal.write_entry(date3,note3)
-    journal.read_entry(date3)
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            user_date = input("Date: ")
+            print("enter your jounral below :-")
+            print('-'*108)
+            user_journal = input()
+            journal = Journal()
+            journal.write_entry(user_date,user_journal)
+            print('-'*108)
+            print("Entry Saved.\n")
+            break
+
+        elif choice == '2':
+            query_date = input("Query Date: ")
+            journal = Journal()
+            query = journal.read_entry(query_date)
+            if query != None:
+                print('-'*108)
+                print(query_date, end='\n\n')
+                print(query)
+                print('-'*108)
+            else:
+                print("No entry found on",query_date)
+            break
+
+        elif choice == '3':
+            sys.exit()
 
 
 if __name__ == "__main__":
